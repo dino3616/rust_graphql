c88ff7e275ca9255f319328b6200e09745f748c3
dino3616 <suraimu3616@outlook.jp>
2022-03-13 21:21:49 +0900

    記事内の「スキーマを作る」まで完成.

---
+++ graphql/src/schemas/root.rs
@@ -0,0 +1,20 @@
+use juniper::{
+    // 今回はSubscriptionを使わないので、ダミーの型を使う必要がある.
+    EmptySubscription,
+    RootNode,
+};
+
+// 後々ジェネリクスの引数とかに使うので、型をまとめておく.
+pub type Schema = RootNode<'static, Query, Mutation, EmptySubscription<Context>>;
+
+pub struct Context {
+    // 今回のシリーズではなんの括約もしないtokenﾆｷ.
+    pub token: Option<String>,
+}
+
+// 「GraphQLのコンテキスト」という特徴を付与する.
+impl juniper::Context for Context {}
+
+pub struct Query;
+
+pub struct Mutation;
