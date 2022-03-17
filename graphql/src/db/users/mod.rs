3194476c7e0d3e25f92d0190955f0fb01f199a56
dino3616 <suraimu3616@outlook.jp>
2022-03-17 15:20:59 +0900

    記事内の「テーブルに関するリポジトリを作成する」まで完成.

---
+++ graphql/src/db/users/mod.rs
@@ -0,0 +1,37 @@
+use crate::db::schema::users;
+use chrono::{
+    DateTime,
+    NaiveDateTime,
+    offset::Local,
+};
+
+mod repository;
+
+// Identifiable: この構造体がDBのテーブルであることを示す.
+// Queryable: この構造体がDBに問い合わせることができることを示す.
+// Clone: おまけ.
+#[derive(Clone, Identifiable, Queryable)]
+pub struct User {
+    pub id: i32,
+    pub name: String,
+    pub profile: Option<String>,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
+
+// Insertable: この構造体がDBに新しい行を挿入できることを示す.
+#[derive(Insertable)]
+#[table_name = "users"]
+pub struct UserNewForm {
+    pub name: String,
+    pub profile: String,
+}
+
+// AsChangeset: この構造体がDBの任意の行に変更を加えられることを示す.
+#[derive(AsChangeset)]
+#[table_name = "users"]
+pub struct UserUpdateForm {
+    pub name: Option<String>,
+    pub profile: Option<String>,
+    pub updated_at: Option<DateTime<Local>>,
+}
---
+++ graphql/src/db/users/repository.rs
@@ -0,0 +1,99 @@
+use crate::db::{
+    PgPool,
+    users::{
+        User,
+        UserNewForm,
+        UserUpdateForm,
+    },
+    schema::users::dsl::*,
+};
+use actix_web::web::Data;
+use anyhow::Result;
+use diesel::{
+    debug_query,
+    dsl::{
+        delete,
+        insert_into,
+        update,
+    },
+    pg::Pg,
+    prelude::*,
+};
+use log::debug;
+
+pub struct Repository;
+
+impl Repository {
+    // 全てのUserを配列として返す.
+    pub fn all(pool: &Data<PgPool>) -> Result<Vec<User>> {
+        let connection = pool.get()?;
+
+        Ok(users.load(&connection)?)
+    }
+
+    // primary keyの配列から、これに合致するUserを配列として返す.
+    pub fn any(pool: &Data<PgPool>, keys: &[i32]) -> Result<Vec<User>> {
+        let connection = pool.get()?;
+        let query = users.filter(id.eq_any(keys));
+
+        let sql = debug_query::<Pg, _>(&query).to_string();
+        debug!("{}", sql);
+
+        Ok(query.get_results(&connection)?)
+    }
+
+    // key_idに合致するUserを返す.
+    pub fn find_by_id(pool: &Data<PgPool>, key_id: i32) -> Result<User> {
+        let connection = pool.get()?;
+        let query = users.find(key_id);
+
+        let sql = debug_query::<Pg, _>(&query).to_string();
+        debug!("{}", sql);
+
+        Ok(query.get_result(&connection)?)
+    }
+
+    // key_nameに合致するUserを配列として返す.
+    pub fn find_by_name(pool: &Data<PgPool>, key_name: String) -> Result<Vec<User>> {
+        let connection = pool.get()?;
+        let query = users.filter(name.eq(key_name));
+
+        let sql = debug_query::<Pg, _>(&query).to_string();
+        debug!("{}", sql);
+
+        Ok(query.get_results(&connection)?)
+    }
+
+    // new_formを新しい行としてDBに追加し、その行のUserを返す.
+    pub fn insert(pool: &Data<PgPool>, new_form: UserNewForm) -> Result<User> {
+        let connection = pool.get()?;
+        let query = insert_into(users).values(new_form);
+
+        let sql = debug_query::<Pg, _>(&query).to_string();
+        debug!("{}", sql);
+
+        Ok(query.get_result(&connection)?)
+    }
+
+    // key_idに合致するUserの行をupdate_formで更新し、その行のUserを返す.
+    pub fn update(pool: &Data<PgPool>, key_id: i32, update_form: UserUpdateForm) -> Result<User> {
+        let connection = pool.get()?;
+        let query = update(users.filter(id.eq(key_id))).set(update_form);
+
+        let sql = debug_query::<Pg, _>(&query).to_string();
+        debug!("{}", sql);
+
+        Ok(query.get_result(&connection)?)
+    }
+
+    // idに合致するUserの行をDBから削除し、その行のUserを返す.
+    pub fn delete(pool: &Data<PgPool>, key_id: i32) -> Result<User> {
+        let connection = pool.get()?;
+        let query = delete(users.find(key_id));
+
+        let sql = debug_query::<Pg, _>(&query).to_string();
+        debug!("{}", sql);
+
+        Ok(query.get_result(&connection)?)
+    }
+}
